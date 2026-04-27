6ac458fac1328874ac0b654a5d1d91b9ce482e5f
kabincs9 <kabinbaniya9@gmail.com>
2026-04-27 23:12:33 +0545

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
-
+from analytics import analytics #Additional 
+app.register_blueprint(analytics)
 # Configure application
 app = Flask(__name__)
 
